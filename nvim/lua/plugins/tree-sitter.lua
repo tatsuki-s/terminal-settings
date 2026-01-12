4593a0656b71c94e9d29189d11b9051ee16e426f
tatsuki-s <zekousn@gmail.com>
2026-01-12 16:44:47 +0900

    setup tree-sitter

---
+++ nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "vue"},
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    local install = require("nvim-treesitter.install")
+    install.compilers = { "gcc" }
+    install.prefer_git = true
+
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if ok then
+      configs.setup(opts)
+    end
+  end
+}
